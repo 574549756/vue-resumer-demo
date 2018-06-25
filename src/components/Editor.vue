14439af849cef92c1f41a32e42040a556004b690
574549756 <574549756@qq.com>
2018-06-26 00:47:50 +0800

    封装自我介绍，添加工作经历添加删除功能

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -11,24 +11,27 @@
 		</nav>
 		<ol class="panes">
 			<li v-bind:class="{active: currentTab === 0}">
-				<h2>个人信息</h2>
-				<el-form>
-					<el-form-item label="姓名">
-						<el-input v-model="profile.name"></el-input>
-					</el-form-item>
-					<el-form-item label="城市">
-						<el-input v-model="profile.city"></el-input>
-					</el-form-item>
-					<el-form-item label="出生年月">
-						<el-input v-model="profile.birth"></el-input>
-					</el-form-item>
-				</el-form>
+				<profileEditor v-bind:profile="profile"/>
 			</li>
 			<li v-bind:class="{active: currentTab === 1}">
 				<h2>学习经历</h2>
 			</li>
 			<li v-bind:class="{active: currentTab === 2}">
 				<h2>工作经历</h2>
+				<hr class="start">
+				<el-form>
+					<div class="container" v-for="(work, index) in jobExperience">
+						<el-form-item label="公司">
+							<el-input v-model="work.company"></el-input>
+						</el-form-item>
+						<el-form-item label="工作内容"></el-form-item>
+						<el-input v-model="work.content"></el-input>
+						</el-form-item>
+						<i class="el-icon-remove" v-on:click="removeJobExperience(index)"></i>
+						<hr class="end">
+					</div>
+						<i class="el-icon-circle-plus" v-on:click="addJobExperience"></i>					
+				</el-form>
 			</li>
 			<li v-bind:class="{active: currentTab === 3}">
 				<h2>项目作品</h2>
@@ -44,75 +47,129 @@
 </template>
 
 <script>
+import profileEditor from './profileEditor.vue'
 export default {
-	data() {
-		return {
-			currentTab: 0,
-			icons: [
-				"credentials_icon",
-				"graduate",
-				"work0",
-				"project",
-				"achievementregular",
-				"phone"
-			],
-			profile: {
-				name: '',
-				city: '',
-				birth: '',
-			}
-		};
-	},
-};
+  components: { profileEditor },
+  data() {
+    return {
+      currentTab: 0,
+      icons: [
+        'credentials_icon',
+        'graduate',
+        'work0',
+        'project',
+        'achievementregular',
+        'phone'
+      ],
+      profile: {
+        name: '',
+        city: '',
+        birth: ''
+      },
+      jobExperience: [
+        {
+          company: '',
+          content: ''
+        }
+      ]
+    }
+  },
+  methods: {
+    addJobExperience() {
+      this.jobExperience.push({
+        company: '',
+        content: ''
+      })
+    },
+    removeJobExperience(index) {
+      this.jobExperience.splice(index, 1)
+    }
+  }
+}
 </script>
 
 
 <style lang="scss">
 $designWidth: 1920;
 @function px($px) {
-    @return $px/$designWidth * 10 + rem;
+  @return $px/$designWidth * 10 + rem;
 }
+
 #editor {
-    min-height: px(100);
-    display: flex;
-    overflow: hidden;
-    .sideNav {
-        background: #000;
-        width: px(100);
-        > ol {
-            .icon {
-                width: px(36);
-                height: px(36);
-                vertical-align: -0.15em;
-                overflow: hidden;
-                margin: px(16) px(10);
-            }
-            :nth-child(1) {
-                .icon {
-                    width: px(40);
-                    height: px(40);
-                    margin: px(12) px(8) px(10) px(8);
-                }
-            }
-            li {
-                text-align: center;
-                &.active {
-                    background: white;
-                    > .icon {
-                        fill: black;
-                    }
-                }
-            }
+  min-height: px(100);
+  display: flex;
+  overflow: hidden;
+  .sideNav {
+    background: #000;
+    width: px(100);
+    > ol {
+      .icon {
+        width: px(36);
+        height: px(36);
+        vertical-align: -0.15em;
+        overflow: hidden;
+        margin: px(16) px(10);
+      }
+      :nth-child(1) {
+        .icon {
+          width: px(40);
+          height: px(40);
+          margin: px(12) px(8) px(10) px(8);
         }
-    }
-    > .panes {
-        > li {
-            display: none;
-			padding: px(32);
-            &.active {
-                display: block;
-            }
+      }
+      li {
+        text-align: center;
+        &.active {
+          background: white;
+          > .icon {
+            fill: black;
+          }
         }
+      }
     }
+  }
+  > .panes {
+    flex: 1;
+    .active {
+      h2 {
+        margin: px(20) px(0);
+      }
+      .el-form {
+        position: relative;
+        .el-icon-circle-plus {
+          transform: scale(1.2);
+          cursor: pointer;
+          position: absolute;
+          right: px(0);
+          bottom: px(-26);
+        }
+      }
+    }
+    .container {
+      position: relative;
+      > .start {
+        margin: px(30) px(0);
+      }
+      > .end {
+        margin: px(40) px(0) px(5) px(0);
+      }
+      .el-icon-remove {
+        transform: scale(1.2);
+        position: absolute;
+        right: px(0);
+        top: px(10);
+        cursor: pointer;
+      }
+    }
+    > li {
+      display: none;
+      padding: px(32);
+      height: 100%;
+      overflow: auto;
+      &.active {
+        display: block;
+      }
+    }
+  }
 }
 </style>
