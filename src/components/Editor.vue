f819c0a895e013f845f067d6d8695f3b8cf8d3b2
574549756 <574549756@qq.com>
2018-06-26 01:00:37 +0800

    jobExperienceEditor.vue has been add in this project.

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -17,21 +17,7 @@
 				<h2>学习经历</h2>
 			</li>
 			<li v-bind:class="{active: currentTab === 2}">
-				<h2>工作经历</h2>
-				<hr class="start">
-				<el-form>
-					<div class="container" v-for="(work, index) in jobExperience">
-						<el-form-item label="公司">
-							<el-input v-model="work.company"></el-input>
-						</el-form-item>
-						<el-form-item label="工作内容"></el-form-item>
-						<el-input v-model="work.content"></el-input>
-						</el-form-item>
-						<i class="el-icon-remove" v-on:click="removeJobExperience(index)"></i>
-						<hr class="end">
-					</div>
-						<i class="el-icon-circle-plus" v-on:click="addJobExperience"></i>					
-				</el-form>
+				<jobExperienceEditor v-bind:jobExperience = "jobExperience"/>
 			</li>
 			<li v-bind:class="{active: currentTab === 3}">
 				<h2>项目作品</h2>
@@ -48,8 +34,9 @@
 
 <script>
 import profileEditor from './profileEditor.vue'
+import jobExperienceEditor from './jobExperienceEditor.vue'
 export default {
-  components: { profileEditor },
+  components: { profileEditor, jobExperienceEditor },
   data() {
     return {
       currentTab: 0,
@@ -74,17 +61,7 @@ export default {
       ]
     }
   },
-  methods: {
-    addJobExperience() {
-      this.jobExperience.push({
-        company: '',
-        content: ''
-      })
-    },
-    removeJobExperience(index) {
-      this.jobExperience.splice(index, 1)
-    }
-  }
+  methods: {}
 }
 </script>
 
---
+++ src/components/jobExperienceEditor.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h2>工作经历</h2>
+		<hr class="start">
+		<el-form>
+			<div class="container" v-for="(work, index) in jobExperience">
+				<el-form-item label="公司">
+					<el-input v-model="work.company"></el-input>
+				</el-form-item>
+				<el-form-item label="工作内容"></el-form-item>
+				<el-input v-model="work.content"></el-input>
+				</el-form-item>
+				<i class="el-icon-remove" v-on:click="removeJobExperience(index)"></i>
+				<hr class="end">
+			</div>
+			<i class="el-icon-circle-plus" v-on:click="addJobExperience"></i>
+		</el-form>
+	</div>
+</template>
+
+<script>
+export default {
+  props: ['jobExperience'],
+  methods: {
+    addJobExperience() {
+      this.jobExperience.push({
+        company: '',
+        content: ''
+      })
+    },
+    removeJobExperience(index) {
+      this.jobExperience.splice(index, 1)
+    }
+  }
+}
+</script>
